824144e4feca23138a33be8c013ed9557d537928
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-05-10 00:12:16 +1000

    Update

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -1,20 +1,119 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput,
+  Image,
+  Platform
+} from "react-native";
+import * as Permissions from "expo-permissions";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import * as ImagePicker from "expo-image-picker";
 
 export default class PostScreen extends React.Component {
+  state = {
+    text: "",
+    image: null
+  };
+
+  componentDidMount() {
+    this.getPhotoPermission();
+  }
+
+  getPhotoPermission = async () => {
+    if (Platform.OS === "ios") {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+      if (status != "granted") {
+        alert(
+          "We need permission to use your camera roll if you'd like to incude a photo."
+        );
+      }
+    }
+  };
+
+  pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3]
+    });
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Post Screen</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+            <Ionicons name="md-arrow-back" size={24} color="#D8D9DB" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.handlePost}>
+            <Text style={{ fontWeight: "500" }}>Post</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Image
+            source={require("../assets/tempAvatar.jpg")}
+            style={styles.avatar}
+          />
+          <TextInput
+            autoFocus={true}
+            multiline={true}
+            numberOfLines={4}
+            style={{ flex: 1 }}
+            placeholder="Want to share something?"
+            onChangeText={text => this.setState({ text })}
+            value={this.state.text}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.photo} onPress={this.pickImage}>
+          <Ionicons name="md-camera" size={32} color="#D8D9DB" />
+        </TouchableOpacity>
+
+        <View style={{ marginHorizontal: 32, marginTop: 32, height: 150 }}>
+          <Image
+            source={{ uri: this.state.image }}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 32,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#D8D9DB"
+  },
+  inputContainer: {
+    margin: 32,
+    flexDirection: "row"
+  },
+  avatar: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    marginRight: 16
+  },
+  photo: {
+    alignItems: "flex-end",
+    marginHorizontal: 32
   }
 });
