8477f916a9d6b28910d5945309e977ba7b91017e
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-05-09 17:52:12 +1000

    Update

--- App.js
+++ App.js
@@ -18,8 +18,6 @@ import RegisterScreen from "./screens/RegisterScreen";
 
 import HomeScreen from "./screens/HomeScreen";
 
-import { Platform, StyleSheet, Text, View } from "react-native";
-
 import firebase from "@react-native-firebase/app";
 
 // TODO(you): import any additional firebase services that you require for your app, e.g for auth:
@@ -29,51 +27,24 @@ import firebase from "@react-native-firebase/app";
 //    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
 //    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\nCmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\nShake or press menu button for dev menu"
+const AppStack = createStackNavigator({
+  Home: HomeScreen
 });
 
-const firebaseCredentials = Platform.select({
-  ios: "https://invertase.link/firebase-ios",
-  android: "https://invertase.link/firebase-android"
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen
 });
 
-type Props = {};
-
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        {!firebase.apps.length && (
-          <Text style={styles.instructions}>
-            {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
-          </Text>
-        )}
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
+export default createAppContainer (
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+);
--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import auth from "@react-native-firebase/auth";
 
 export default class LoadingScreen extends React.Component {
+  componentDidMount() {
+    auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? "App" : "Auth");
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Loading Screen</Text>
+        <Text>Loading...</Text>
+        <ActivityIndicator size="large" />
       </View>
     );
   }
