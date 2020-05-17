d71e560ff72de836cdd51ff03ccc700e8b39905f
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-05-17 17:11:25 +1000

    Update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Agenda } from "react-native-calendars";
 
 export default class HomeScreen extends React.Component {
+  state = {
+    items: {}
+  };
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity style={styles.item}>
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-      </View>
+      <Agenda
+        items={this.state.items}
+        renderItem={this.renderItem.bind(this)}
+        pastScrollRange={1}
+        futureScrollRange={2}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  item: {
+    backgroundColor: "white",
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
   }
 });
