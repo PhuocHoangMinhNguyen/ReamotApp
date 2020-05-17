2459506604f7d4dcd3ed54472534da0c40c68f75
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-05-17 17:55:02 +1000

    Update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,49 @@ export default class HomeScreen extends React.Component {
     items: {}
   };
 
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {
+        newItems[key] = this.state.items[key];
+      });
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  }
+
   renderItem(item) {
     return (
       <TouchableOpacity style={styles.item}>
@@ -19,7 +62,10 @@ export default class HomeScreen extends React.Component {
     return (
       <Agenda
         items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
         renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
         pastScrollRange={1}
         futureScrollRange={2}
       />
