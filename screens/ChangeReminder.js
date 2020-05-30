0fa5112f3d7731f36bdc8f6c81452ddbcf28e8e5
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-05-30 23:57:40 +1000

    Update

--- screens/ChangeReminder.js
+++ screens/ChangeReminder.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Image, Button } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import Ionicons from "react-native-vector-icons/Ionicons"
 import Alarm from "../utilities/Alarm"
 
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -41,21 +41,6 @@ export default class ProfileScreen extends React.Component {
           </View>
           <Text style={styles.name}>{this.state.user.name}</Text>
         </View>
-        <View style={styles.statsContainer}>
-          <View style={styles.stat}>
-            <Text style={styles.statAmount}>21</Text>
-            <Text style={styles.statTitle}>Posts</Text>
-          </View>
-          <View style={styles.stat}>
-            <Text style={styles.statAmount}>981</Text>
-            <Text style={styles.statTitle}>Followers</Text>
-          </View>
-          <View style={styles.stat}>
-            <Text style={styles.statAmount}>63</Text>
-            <Text style={styles.statTitle}>Following</Text>
-          </View>
-        </View>
-
         <Button style={styles.button}
           onPress={() => {
             auth().signOut();
@@ -88,27 +73,8 @@ const styles = StyleSheet.create({
   name: {
     marginTop: 24,
     fontSize: 16,
-    fontWeight: "600"
-  },
-  statsContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    margin: 32
-  },
-  stat: {
-    alignItems: "center",
-    flex: 1
-  },
-  statAmount: {
-    color: "#4F566D",
-    fontSize: 18,
-    fontWeight: "300"
-  },
-  statTitle: {
-    color: "#C3C5CD",
-    fontSize: 12,
-    fontWeight: "500",
-    marginTop: 4
+    fontWeight: "600",
+    marginBottom: 24
   },
   button: {
     color: "#018ABE"
