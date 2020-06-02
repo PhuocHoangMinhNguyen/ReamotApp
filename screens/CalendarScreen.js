465d450e7c7e4ebb3493d9f454686c35207b78ce
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-06-02 21:54:06 +1000

    Update

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,166 +1,88 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, FlatList } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import moment from "moment";
-import { Calendar } from "react-native-calendars";
-
-// temporary data until we pull from Firebase
-var posts = [
-  {
-    id: "1",
-    name: "Joe McKay",
-    text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    timestamp: 1569109273726,
-    avatar: require("../assets/tempAvatar.jpg"),
-    image: require("../assets/tempImage1.jpg")
-  },
-  {
-    id: "2",
-    name: "Karyn Kim",
-    text:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    timestamp: 1569109273726,
-    avatar: require("../assets/tempAvatar.jpg"),
-    image: require("../assets/tempImage2.jpg")
-  },
-  {
-    id: "3",
-    name: "Emerson Parsons",
-    text:
-      "Amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant.",
-    timestamp: 1569109273726,
-    avatar: require("../assets/tempAvatar.jpg"),
-    image: require("../assets/tempImage3.jpg")
-  },
-  {
-    id: "4",
-    name: "Kathie Malone",
-    text:
-      "At varius vel pharetra vel turpis nunc eget lorem. Lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor. Adipiscing tristique risus nec feugiat in fermentum.",
-    timestamp: 1569109273726,
-    avatar: require("../assets/tempAvatar.jpg"),
-    image: require("../assets/tempImage4.jpg")
-  }
-];
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Agenda } from "react-native-calendars";
 
 export default class CalendarScreen extends React.Component {
-  renderPost = post => {
-    return (
-      <View style={styles.feedItem}>
-        <Image source={post.avatar} style={styles.avatar} />
-        <View style={{ flex: 1 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center"
-            }}
-          >
-            <View>
-              <Text style={styles.name}>{post.name}</Text>
-              <Text style={styles.timestamp}>
-                {moment(post.timestamp).fromNow()}
-              </Text>
-            </View>
+  state = {
+    items: {}
+  };
 
-            <Ionicons name="ios-more" size={24} color="#73788B" />
-          </View>
-          <Text style={styles.post}>{post.text}</Text>
-          <Image
-            source={post.image}
-            style={styles.postImage}
-            resizeMode="cover"
-          />
-          <View style={{ flexDirection: "row" }}>
-            <Ionicons
-              name="ios-heart-empty"
-              size={24}
-              color="#73788B"
-              style={{ marginRight: 16 }}
-            />
-            <Ionicons name="ios-chatboxes" size={24} color="#73788B" />
-          </View>
-        </View>
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {
+        newItems[key] = this.state.items[key];
+      });
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
       </View>
     );
-  };
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity style={styles.item}>
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Calendar minDate={new Date()} />
-
-        <FlatList
-          style={styles.feed}
-          data={posts}
-          renderItem={({ item }) => this.renderPost(item)}
-          keyExtractor={item => item.id}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        pastScrollRange={1}
+        futureScrollRange={2}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  item: {
+    backgroundColor: "white",
     flex: 1,
-    backgroundColor: "#EBECF4"
-  },
-  header: {
-    paddingTop: 64,
-    paddingBottom: 16,
-    backgroundColor: "#FFF",
-    alignItems: "center",
-    justifyContent: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: "#EBECF4",
-    shadowColor: "#454D65",
-    shadowOffset: { height: 5 },
-    shadowRadius: 15,
-    shadowOpacity: 0.2,
-    zIndex: 10
-  },
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: "500"
-  },
-  feed: {
-    marginHorizontal: 16
-  },
-  feedItem: {
-    backgroundColor: "#FFF",
     borderRadius: 5,
-    padding: 8,
-    flexDirection: "row",
-    marginVertical: 8
-  },
-  avatar: {
-    width: 36,
-    height: 36,
-    borderRadius: 18,
-    marginRight: 16
-  },
-  name: {
-    fontSize: 15,
-    fontWeight: "500",
-    color: "#454D65"
-  },
-  timestamp: {
-    fontSize: 11,
-    color: "#C4C6CE",
-    marginTop: 4
-  },
-  post: {
-    marginTop: 16,
-    fontSize: 14,
-    color: "#838899"
-  },
-  postImage: {
-    width: undefined,
-    height: 150,
-    borderRadius: 5,
-    marginVertical: 16
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
   }
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,88 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { Agenda } from "react-native-calendars";
+import { View, Text, StyleSheet } from "react-native";
 
 export default class HomeScreen extends React.Component {
-  state = {
-    items: {}
-  };
-
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
-  }
-
-  rowHasChanged(r1, r2) {
-    return r1.name !== r2.name;
-  }
-
-  loadItems(day) {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: "Item for " + strTime + " #" + j,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => {
-        newItems[key] = this.state.items[key];
-      });
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-  }
-
-  renderEmptyDate() {
-    return (
-      <View style={styles.emptyDate}>
-        <Text>This is empty date!</Text>
-      </View>
-    );
-  }
-
-  renderItem(item) {
-    return (
-      <TouchableOpacity style={styles.item}>
-        <Text>{item.name}</Text>
-      </TouchableOpacity>
-    );
-  }
-
   render() {
     return (
-      <Agenda
-        items={this.state.items}
-        loadItemsForMonth={this.loadItems.bind(this)}
-        renderItem={this.renderItem.bind(this)}
-        renderEmptyDate={this.renderEmptyDate.bind(this)}
-        rowHasChanged={this.rowHasChanged.bind(this)}
-        pastScrollRange={1}
-        futureScrollRange={2}
-      />
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  item: {
-    backgroundColor: "white",
     flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    marginTop: 17
-  }
+    alignItems: "center",
+    justifyContent: "center"
+  },
 });
