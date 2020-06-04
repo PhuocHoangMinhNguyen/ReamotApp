0b85499800ec343b499992ac10f2259fca453099
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-06-04 14:10:01 +1000

    Update

--- screens/DoctorInfoScreen.js
+++ screens/DoctorInfoScreen.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Image, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import Toast from "react-native-simple-toast";
 
 export default class DoctorInfoScreen extends React.Component {
     static navigationOptions = {
         headerShown: false,
     };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            doctor: {},
+        };
+    }
+
+    componentDidMount() {
+        //Get data from Medicine Screen
+        let paramsFromDoctorScreen = this.props.navigation.state.params;
+        this.setState({ doctor: paramsFromDoctorScreen });
+    }
+
+    handleDetails = () => {
+        Toast.show("Your medical details is sent to this individual !")
+    }
+
+    handleSchedule = () => {
+        Toast.show("Your appointment is set !")
+    }
 
     render() {
         return (
@@ -16,8 +38,29 @@ export default class DoctorInfoScreen extends React.Component {
                 >
                     <Ionicons name="ios-arrow-round-back" size={32} color="#FFF" />
                 </TouchableOpacity>
-                <Text>Doctor Information Screen</Text>
-            </View>
+                <View style={styles.information}>
+                    <Image
+                        source={
+                            this.state.doctor.avatar
+                                ? { uri: this.state.doctor.avatar }
+                                : require("../assets/tempAvatar.jpg")
+                        }
+                        style={styles.image} />
+                    <View style={{ alignItems: "center" }}>
+                        <Text style={{ fontSize: 18 }}>{this.state.doctor.name}</Text>
+                    </View>
+                </View>
+                <View style={{ marginVertical: 5 }}>
+                    <Button
+                        title="Give access of medical details"
+                        onPress={this.handleDetails} />
+                </View>
+                <View style={{ marginVertical: 5 }}>
+                    <Button
+                        title="Schedule An Appointment"
+                        onPress={this.handleSchedule} />
+                </View>
+            </View >
         );
     }
 }
@@ -38,5 +81,16 @@ const styles = StyleSheet.create({
         backgroundColor: "rgba(21, 22, 48, 0.1)",
         alignItems: "center",
         justifyContent: "center",
+    },
+    information: {
+        backgroundColor: "#FFF",
+        borderRadius: 5,
+        padding: 16,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    image: {
+        width: 100,
+        height: 100
     },
 });
