c7b8a5fae4ffc2436fe59d29d418e8aa4b24cbbe
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-08-07 17:51:11 +1000

    Update (7/8)

--- screens/MoreStack/HelpScreen.js
+++ screens/MoreStack/HelpScreen.js
@@ -9,7 +9,15 @@ export default class HelpScreen extends React.Component {
                     <Text style={styles.questionText}>About Us</Text>
                 </View>
                 <View style={styles.answerSection}>
-                    <Text style={styles.answerText}></Text>
+                    <Text style={styles.answerText}>We are a group of 5 last-year La Trobe students,
+                    collaboration with Aerion Technologies, wanting to make some contributions to improve people’s life.</Text>
+                    <Text></Text>
+                    <Text style={styles.answerText}>Contributors:</Text>
+                    <Text style={styles.answerText}>-    Phuoc Hoang Minh Nguyen</Text>
+                    <Text style={styles.answerText}>-    Quang Duy Nguyen</Text>
+                    <Text style={styles.answerText}>-    Yaqdhan Sulaiman Fraish Al Mahrizi</Text>
+                    <Text style={styles.answerText}>-    Jander Yang</Text>
+                    <Text style={styles.answerText}>-    Joanne Scott</Text>
                 </View>
 
                 <View style={styles.questionSection}>
@@ -18,10 +26,12 @@ export default class HelpScreen extends React.Component {
                 <View style={styles.answerSection}>
                     <Text style={styles.answerText}>It has been found that up to 50% of patients with prescription medicines
                     do not take the doses as prescribed or have poor adherence.</Text>
+                    <Text></Text>
                     <Text style={styles.answerText}>The objective of Reamot app is to close the loop between the Physicians,
                     Pharmacists and Patients in relation to the accuracy of medication taking and treatment adherence.
                     This solution is a fully integrated app website application that delivers real time data to both the doctor
                     and the Pharmacist.</Text>
+                    <Text></Text>
                     <Text style={styles.answerText}>Patients can now register for a simple and easy to use prescription reminder app that records
                     when and if they correctly take their medication. This data is made available to the patients
                     doctors and pharmacists.</Text>
@@ -31,7 +41,15 @@ export default class HelpScreen extends React.Component {
                     <Text style={styles.questionText}>What can this app do?</Text>
                 </View>
                 <View style={styles.answerSection}>
-                    <Text style={styles.answerText}></Text>
+                    <Text style={styles.answerText}>Set up reminder to remind yourself to take some certain medicines, based on doctor’s prescription,
+                    or you can add your own medicine with your own instructions.</Text>
+                    <Text></Text>
+                    <Text style={styles.answerText}>Revise your own history of taking medications</Text>
+                    <Text></Text>
+                    <Text style={styles.answerText}>Give access to doctors and pharmacists who using the system
+                    so they can constantly keep track of your medication adherence.</Text>
+                    <Text></Text>
+                    <Text style={styles.answerText}>Make appointments with doctors.</Text>
                 </View>
 
                 <View style={styles.questionSection}>
