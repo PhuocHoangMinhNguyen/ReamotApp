1fcf0ac9095b11f5a3c5312bce8c1209eea333b2
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-08-07 17:19:03 +1000

    Update (7/8)

--- screens/MoreStack/HelpScreen.js
+++ screens/MoreStack/HelpScreen.js
@@ -6,35 +6,43 @@ export default class HelpScreen extends React.Component {
         return (
             <View style={styles.container}>
                 <View style={styles.questionSection}>
-                    <Text></Text>
+                    <Text style={styles.questionText}>About Us</Text>
                 </View>
                 <View style={styles.answerSection}>
                     <Text style={styles.answerText}></Text>
                 </View>
 
                 <View style={styles.questionSection}>
-                    <Text>Why do I need this app?</Text>
+                    <Text style={styles.questionText}>Why do you need this app?</Text>
+                </View>
+                <View style={styles.answerSection}>
+                    <Text style={styles.answerText}>It has been found that up to 50% of patients with prescription medicines
+                    do not take the doses as prescribed or have poor adherence.</Text>
+                    <Text style={styles.answerText}>The objective of Reamot app is to close the loop between the Physicians,
+                    Pharmacists and Patients in relation to the accuracy of medication taking and treatment adherence.
+                    This solution is a fully integrated app website application that delivers real time data to both the doctor
+                    and the Pharmacist.</Text>
+                    <Text style={styles.answerText}>Patients can now register for a simple and easy to use prescription reminder app that records
+                    when and if they correctly take their medication. This data is made available to the patients
+                    doctors and pharmacists.</Text>
+                </View>
+
+                <View style={styles.questionSection}>
+                    <Text style={styles.questionText}>What can this app do?</Text>
                 </View>
                 <View style={styles.answerSection}>
                     <Text style={styles.answerText}></Text>
                 </View>
 
                 <View style={styles.questionSection}>
-                    <Text></Text>
+                    <Text style={styles.questionText}></Text>
                 </View>
                 <View style={styles.answerSection}>
                     <Text style={styles.answerText}></Text>
                 </View>
 
                 <View style={styles.questionSection}>
-                    <Text></Text>
-                </View>
-                <View style={styles.answerSection}>
-                    <Text style={styles.answerText}></Text>
-                </View>
-
-                <View style={styles.questionSection}>
-                    <Text></Text>
+                    <Text style={styles.questionText}></Text>
                 </View>
                 <View style={styles.answerSection}>
                     <Text style={styles.answerText}></Text>
@@ -50,7 +58,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#DEE8F1',
     },
     questionSection: {
-        flexDirection: "row",
         backgroundColor: "#FFF",
         borderRadius: 4,
         marginLeft: 30,
@@ -59,13 +66,16 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     answerSection: {
-        flexDirection: "row",
         backgroundColor: "#1565C0",
         borderRadius: 4,
         marginLeft: 70,
         marginRight: 30,
         marginVertical: 8,
         padding: 10,
+    },
+    questionText: {
+        fontWeight: "bold",
+        fontSize: 15
     },
     answerText: {
         color: "#FFF"
