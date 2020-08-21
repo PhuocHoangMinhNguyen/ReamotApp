198212500670cdb64d5b04adc150269c0422426e
Phuoc Hoang Minh Nguyen <nphminh36@gmail.com>
2020-08-21 20:23:40 +1000

    Delete Reminders on Logout (21/08)

--- utilities/UserReminders.js
+++ utilities/UserReminders.js
@@ -3,10 +3,20 @@
 // Delete all reminders when logging out, 
 // and set all reminders when logging in
 // Status: In development
+import ReactNativeAN from 'react-native-alarm-notification'
+import firestore from "@react-native-firebase/firestore"
 
 class UserReminders {
     deleteReminders = () => {
-
+        let temp = []
+        firestore().collection('reminder').onSnapshot(querySnapshot => {
+            querySnapshot.forEach(documentSnapshot => {
+                temp.push(documentSnapshot.data().idAN)
+            })
+            for (let i = 0; i < temp.length; i++) {
+                ReactNativeAN.deleteAlarm(temp[i].toString())
+            }
+        })
     }
     setReminders = () => {
 
